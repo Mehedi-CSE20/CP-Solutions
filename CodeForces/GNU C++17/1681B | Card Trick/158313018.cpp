833056210f8a55a3ae24c53b8d7ef03f2ef42f9d
Mehedi Hasan Maruf <41261534+Mehedi-CSE20@users.noreply.github.com>
2022-07-08 00:28:49 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1681B | Card Trick/158313018.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fastIO   ios_base::sync_with_stdio(0); cin.tie(nullptr);
+ 
+int main() {
+    fastIO
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        int m;
+        cin >> m;
+        int sum{0};
+        for (int i = 0; i < m; i++) {
+            int x;
+            cin >> x;
+            sum += x;
+            if (sum >= n) sum %= n;
+        }
+        cout << a[sum] << '\n';
+    }
+}
