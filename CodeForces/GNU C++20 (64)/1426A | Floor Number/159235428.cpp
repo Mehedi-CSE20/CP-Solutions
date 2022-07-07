7e44ca625c71e92f16f672f2e7b44113e4453203
Mehedi Hasan Maruf <41261534+Mehedi-CSE20@users.noreply.github.com>
2022-07-08 00:26:43 +0600

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1426A | Floor Number/159235428.cpp
@@ -0,0 +1,24 @@
+// Updated algo | python makes us logical KoDer ;P
+ 
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#define foi(a, b, c) for (ll c{(a)}; c < (b); c++)
+#define fod(a, b, c) for (ll (c){(a)}; (c) < (b); (c)--)
+#define ll int64_t
+using namespace std;
+ 
+ 
+int main() {
+    int n{0};
+    cin >> n;
+    while (2*n--) {
+        int a{0}, b{0};
+        cin >> a >> b;
+        if (a <= 2) {
+            cout << 1 << endl;
+            continue;
+        }
+        cout << ((a-3)/b)+2 << endl;
+    }
+}
