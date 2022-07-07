dd1007f04835a9c93e590133bb8b5ada63419ff5
Mehedi Hasan Maruf <41261534+Mehedi-CSE20@users.noreply.github.com>
2022-07-08 00:21:14 +0600

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1543B | Customising the Track/161327662.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define ff first
+#define ss second
+#define nn '\n'
+#define mod 1000000007
+#define read(_a, _n) for (ll i = 0; i < _n; i++) cin >> _a[i];
+#define show(_a) for (auto &val : _a) cout << val << " ";
+#define mx(_a) *max_element(_a.begin(), _a.end())
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define fast   ios_base::sync_with_stdio(0); cin.tie(0);
+ 
+ 
+int main() {
+    fast
+    fast
+    ll tc;
+    cin >> tc;
+    while (tc--) {
+        ll n;
+        ll sum{0};
+        cin >> n;
+        ll a[n]{0};
+        for (ll i = 0; i < n; i++) {
+            cin >> a[i];
+            sum += a[i];
+        }
+        ll rem = sum % n;
+        cout << (n - rem) * (rem) << nn;
+    }
+}
