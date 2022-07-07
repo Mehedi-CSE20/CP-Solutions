55caa39d5710065942e8578b904bc135974d778a
Mehedi Hasan Maruf <41261534+Mehedi-CSE20@users.noreply.github.com>
2022-07-08 00:31:34 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1237B | Balanced Tunnel/153762204.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+#define fastIO        ios_base::sync_with_stdio(0);cin.tie(nullptr);
+ 
+ 
+int main() {
+    fastIO
+    int n{0};
+    cin >> n;
+    int b[n], a[n];
+    for (int i{0}; i < n; i++) {
+         cin >> a[i];
+         a[i]--;
+    }
+    for (int i{0}; i < n; i++) {
+        int temp;
+        cin >> temp;
+        b[temp-1] = i;
+    }
+    int fn{0}, flg{-1};
+    for (auto j = 0; j < n; j++) {
+        //cout << b[a[j]] << ' ';
+        if (b[a[j]] > flg) {
+            //cout << endl;
+            flg = b[a[j]];
+            continue;
+        }
+        fn++;
+    }
+    cout << fn << '\n';
+}
