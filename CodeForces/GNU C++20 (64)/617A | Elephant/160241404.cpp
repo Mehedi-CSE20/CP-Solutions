5e0ec4f47e9055eb196e2a6eefa0589c480df41b
Mehedi Hasan Maruf <41261534+Mehedi-CSE20@users.noreply.github.com>
2022-07-08 00:23:50 +0600

    Committing files

---
+++ CodeForces/GNU C++20 (64)/617A | Elephant/160241404.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#define ff first
+#define ss second
+#define nn '\n'
+using namespace std;
+#define ll long long
+#define fast   ios_base::sync_with_stdio(0); cin.tie(0);
+ 
+int main() {
+    fast
+    int x;
+    cin >> x;
+    cout << ceil(x/5.0) << '\n';
+}
