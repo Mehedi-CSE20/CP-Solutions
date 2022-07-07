cfaf4d5faf28c031925e1af5874204791d477956
Mehedi Hasan Maruf <41261534+Mehedi-CSE20@users.noreply.github.com>
2022-07-08 00:18:01 +0600

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1421C | Palindromifier/163100788.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define ff first
+#define ss second
+#define nn '\n'
+#define prime 1000000007
+#define read(_a, _n) for (ui i = 0; i < _n; i++) cin >> _a[i];
+#define show(_a) for (auto &val : _a) cout << val << " ";
+#define mx(_a) *max_element(_a.begin(), _a.end())
+#define all(_a) _a.begin(), _a.end()
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define ui unsigned ll
+#define fast   ios_base::sync_with_stdio(0); cin.tie(0);
+// Use M_PI for pi
+ 
+int main() {
+    fast
+    string s;
+    cin >> s;
+    int l = s.length();
+    cout << "3\nR " << l - 1 << "\nL " << l << "\nL 2" << nn;
+return 0;
+}
